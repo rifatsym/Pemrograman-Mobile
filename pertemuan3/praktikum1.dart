bf8b23da6cea68fb6269398b913eeca6f0fa5fe4
Muhammad Rifat Syamlan <rifatsyamlan78@gmail.com>
2026-04-12 09:45:49 +0700

    praktikum1

---
+++ pertemuan3/praktikum1.dart
@@ -0,0 +1,12 @@
+void main() {
+  String test = "test2";
+if (test == "test1") {
+  print("test1");
+} else if (test == "test2") {
+  print("test2");
+} else {
+  print("Something else");
+}
+
+if (test == "test2") print("test2 again");
+}

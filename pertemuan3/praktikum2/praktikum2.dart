52bc6771bb3031f9c4116eb0c6e922b0ed746214
Muhammad Rifat Syamlan <rifatsyamlan78@gmail.com>
2026-04-12 09:59:22 +0700

    praktikum 2 langkah 2

--- pertemuan3/praktikum2/praktikum2.dart
+++ pertemuan3/praktikum2/praktikum2.dart
@@ -1,4 +1,8 @@
-while (counter < 33) {
-  print(counter);
-  counter++;
+void main() {
+  int counter = 0; // PERBAIKAN: Mendeklarasikan dan memberi nilai awal 0
+  
+  while (counter < 33) {
+    print(counter);
+    counter++;
+  }
 }

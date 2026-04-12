609da6c89d22554a62d61a9760ffa9b6fdb746d2
Muhammad Rifat Syamlan <rifatsyamlan78@gmail.com>
2026-04-12 10:18:34 +0700

    praktikum3 langkah 3

--- pertemuan3/praktikum3/praktikum3.dart
+++ pertemuan3/praktikum3/praktikum3.dart
@@ -1,6 +1,16 @@
 void main() {
-  // Perbaikan: Tambahkan 'int', samakan huruf kecil semua, dan tambahkan '++'
-  for (int index = 10; index < 27; index++) { 
+  for (int index = 10; index < 27; index++) {
+    
+    // Berhenti total jika mencapai angka 21
+    if (index == 21) {
+      break; 
+    } 
+    // Melewati angka 15, 16, 17, dan 18
+    else if (index >= 15 && index <= 18) { // Mengubah logika di sini
+      continue; 
+    }
+    
+    // Jika tidak terkena break atau continue, cetak angkanya
     print(index);
   }
 }

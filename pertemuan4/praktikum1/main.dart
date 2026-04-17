9138173a9583006625508fdb9582546b6a8c4ab4
Muhammad Rifat Syamlan <rifatsyamlan78@gmail.com>
2026-04-17 13:31:27 +0700

    commit

---
+++ pertemuan4/praktikum1/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  final List<String?> list = List.filled(5, null);
+
+  list[1] = 'Muhammad Rif\'at Syamlan';   
+  list[2] = '244107060150';    
+
+  print(list);       
+  print(list[1]);    
+  print(list[2]);    
+}
